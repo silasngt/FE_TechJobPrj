9e1fd624f75670c69fdc3ba3ce5ef695a62eed66
silas <nguyengiangthanhtai2016@gmail.com>
2025-12-28 22:17:51 +0700

    Adjust nextConfig

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,25 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/user-manage/:path*',
+        destination: '/?redirect=unauthorized',
+        permanent: false,
+      },
+      {
+        source: '/company-manage/:path*',
+        destination: '/?redirect=unauthorized',
+        permanent: false,
+      },
+      {
+        source: '/job/apply/:path*',
+        destination: '/?redirect=unauthorized',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
