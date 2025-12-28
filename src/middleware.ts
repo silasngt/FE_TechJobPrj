9657ffd3a97d19f7e9e4f1b875fda22f3fbbfe22
silas <nguyengiangthanhtai2016@gmail.com>
2025-12-28 22:30:31 +0700

    Adjust middleware for cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,16 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token')?.value;
-  if (token) {
-    return NextResponse.next();
-  } else {
+  const cookieHeader = request.headers.get('cookie') || '';
+
+  // chỉ cần tồn tại token
+  const hasToken = cookieHeader.includes('token=');
+
+  if (!hasToken) {
     return NextResponse.redirect(new URL('/', request.url));
   }
+
+  return NextResponse.next();
 }
 
 export const config = {
